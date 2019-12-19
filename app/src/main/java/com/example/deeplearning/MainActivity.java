2c9544a08c0d02f67111243256729727e76d791c
Saim <saimh355@gmail.com>
2019-12-19 13:25:13 +0500

    DeepLearning in Android

---
+++ app/src/main/java/com/example/deeplearning/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.deeplearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.cameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.INVISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                        default:
+                            super.onManagerConnected(status);
+                            break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()){
+            Toast.makeText(this, "There is a problem ", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (cameraBridgeViewBase != null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase!=null)
+            cameraBridgeViewBase.disableView();
+    }
+}
